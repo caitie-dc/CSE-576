11bf795af14d2102f4475f85517b89f23334a8cd
Caitlin DeShazo-Couchot <83627919+caitie-dc@users.noreply.github.com>
2026-05-29 01:56:03 -0700

    Finished hw5

--- answer.py
+++ answer.py
@@ -35,61 +35,58 @@ class SimpleCNN(nn.Module):
         self.fc1 = nn.Linear(1568, 5)
 
     def forward(self, x):
-        """
-        Question 2
-
-        TODO: fill this forward function for data flow
-        """
-        pass
-
+        x = self.conv_layer(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+        
 
 # %%
 basic_transformer = transforms.Compose([transforms.ToTensor()])
 
-"""
-Question 3
-
-TODO: Add color normalization to the transformer. For simplicity, let us use 0.5 for mean
-      and 0.5 for standard deviation for each color channel.
-"""
-norm_transformer = transforms.Compose([])
+#Add color normalization to the transformer using 0.5 for mean and std for each channel. 
+norm_transformer = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
 
 # %%
 class DeepCNN(nn.Module):
     def __init__(self, arr=[]):
         super(DeepCNN, self).__init__()
-        """
-        Question 4
-
-        TODO: setup the structure of the network
-        """
-        pass
+        size = 30
+        layers = []
+        in_channels = 3
+        last_channels = 3
+        for val in arr:
+            if val == 'pool':
+                layers.append(nn.MaxPool2d(2))
+                size = size // 2
+            else:
+                layers.append(nn.Conv2d(in_channels, val, 3))
+                in_channels = val
+                last_channels = val
+                size = size - 2
+        self.layers = nn.ModuleList(layers)
+        self.fc1 = nn.Linear(size * size * last_channels, 5)
 
     def forward(self, x):
-        """
-        Question 4
-
-        TODO: setup the flow of data (tensor)
-        """
-        pass
+        for layer in self.layers:
+            if isinstance(layer, nn.MaxPool2d):
+                x = layer(x)
+            else:
+                x = layer(x)
+                x = F.relu(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+        
 
 
 # %%
-"""
-Question 5
 
-TODO:
-    change the aug_transformer to a tranformer with random horizontal flip
-    and random affine transformation
-
-    1. It should randomly flip the image horizontally with probability 50%
-    2. It should apply random affine transformation to the image, which randomly rotate the image 
-        within 5 degrees, and shear the image within 10 degrees.
-    3. It should include color normalization after data augmentation. Similar to question 3.
-"""
-
-"""Add random data augmentation to the transformer"""
-aug_transformer = transforms.Compose([])
-
-
+aug_transformer = transforms.Compose([transforms.ToTensor(),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomAffine(degrees=5, shear=10),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
